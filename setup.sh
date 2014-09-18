89b01dd17abc4cd2b13fe86e7add68883ed1913d
Sam Hanes <sam@maltera.com>
2014-09-18 03:02:41 -0700

    Add a quick setup script.

---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cd $HOME
+
+move_link () {
+    if [[ -e "$2" ]]; then
+        mv "$2" "$2.old"
+    fi
+
+    ln -s "$1" "$2"
+}
+
+normal () {
+    move_link ".files/$1" ".$1"
+}
+
+special () {
+    move_link ".files/$1" "$2"
+}
+
+
+normal bash_logout
+normal bashrc
+normal hgignore
+normal hgrc
+normal profile
+normal vim
+normal Xresources
+
+move_link .vim/vimrc .vimrc
+
+mkdir -p .xmonad
+special xmonad.hs .xmonad/xmonad.hs
