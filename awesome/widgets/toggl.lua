307193d714abc93811000198d32fd8526cda3741
Sam Hanes <sam@fullstacklabs.co>
2019-12-18 20:37:31 -0800

    awesome: show seconds for current Toggl entry.

--- awesome/widgets/toggl.lua
+++ awesome/widgets/toggl.lua
@@ -10,11 +10,20 @@ local fs = gears.filesystem
 local toggl = require("toggl")
 
 
-local function format_time(seconds)
+local function format_time_min(seconds)
     return string.format(
         "%02d:%02d",
         math.floor(seconds / 3600),
         math.floor(seconds % 3600 / 60)
+    )
+end
+
+local function format_time_sec(seconds)
+    return string.format(
+        "%02d:%02d:%02d",
+        math.floor(seconds / 3600),
+        math.floor(seconds % 3600 / 60),
+        math.floor(seconds % 60)
     )
 end
 
@@ -41,20 +50,20 @@ return function()
     timer:connect_signal("timeout", function()
         local total = toggl:getTotalTimeToday()
         if total >= 8 * 60 * 60 then
-            text_today.markup = '<span color="#22aa22">' .. format_time(total) .. '</span>'
+            text_today.markup = '<span color="#22aa22">' .. format_time_min(total) .. '</span>'
         else
-            text_today.markup = format_time(total)
+            text_today.markup = format_time_min(total)
         end
 
         local current = toggl:getCurrentEntryTime()
         if current == nil then
-            text_current.markup = '<span color="#555555">00:00</span>'
+            text_current.markup = '<span color="#555555">00:00:00</span>'
         elseif current >= 3 * 60 * 60 then
-            text_current.markup = '<span color="#dd2222">' .. format_time(current) .. '</span>'
+            text_current.markup = '<span color="#dd2222">' .. format_time_sec(current) .. '</span>'
         elseif current >= 2 * 60 * 60 then
-            text_current.markup = '<span color="#cc9900">' .. format_time(current) .. '</span>'
+            text_current.markup = '<span color="#cc9900">' .. format_time_sec(current) .. '</span>'
         else
-            text_current.markup = format_time(current)
+            text_current.markup = format_time_sec(current)
         end
     end)
     timer:start()
