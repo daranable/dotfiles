5157bcdb205e1aa122d5d60eba9c2b5c7214a419
Sam Hanes <sam@fullstacklabs.co>
2019-12-18 18:59:44 -0800

    Awesome: port Toggl from lua-http to libsoup via lgi.

--- awesome/toggl.lua
+++ awesome/toggl.lua
@@ -2,9 +2,11 @@ local _ENV = require("stdlib")
 
 local gears = require("gears")
 
+local lgi = require("lgi")
+local soup = lgi.require("Soup")
+
 local basexx = require("basexx")
 local json = require("json")
-local request = require("http.request")
 
 
 local API_URL = "https://api_token@www.toggl.com"
@@ -19,6 +21,21 @@ local function datetime_3339(time)
     return string.sub(date, 0, -3) .. ":" .. string.sub(date, -2)
 end
 
+local function today_3339()
+    local time = os.date("*t")
+
+    -- the midnight preceding today
+    time.hour = 0
+    time.min = 0
+    time.sec = 0
+    local start_date = datetime_3339(time)
+
+    -- the midnight following today
+    time.day = time.day + 1
+    local end_date = datetime_3339(time)
+
+    return start_date, end_date
+end
 
 function toggl:start()
     if self._timer then return end
@@ -33,6 +50,10 @@ function toggl:start()
         self._ignore_projects[id] = true
     end
 
+    self._soup = soup.Session()
+
+    self._entries = {}
+
     self._timer = gears.timer({timeout = 5 * 60})
     self._timer:connect_signal("timeout", function()
         self:poll()
@@ -42,42 +63,32 @@ function toggl:start()
 end
 
 function toggl:poll()
-    local time = os.date("*t")
+    local start_date, end_date = today_3339()
 
-    -- the midnight preceding today
-    time.hour = 0
-    time.min = 0
-    time.sec = 0
-    local start_date = datetime_3339(time)
-
-    -- the midnight following today
-    time.day = time.day + 1
-    local end_date = datetime_3339(time)
-
-    local req = request.new_from_uri(
+    local msg = soup.Message.new(
+        "GET",
         API_URL .. "/api/v8/time_entries"
         .. "?start_date=" .. start_date
         .. "&end_date=" .. end_date
     )
 
-    req.headers:upsert(
-        "authorization",
-        "basic " .. basexx.to_base64(self._config.token .. ":api_token"),
-        true
+    msg.request_headers:replace(
+        "Authorization",
+        "Basic " .. basexx.to_base64(self._config.token .. ":api_token")
     )
 
-    local headers, stream = assert(req:go())
-    local body = assert(stream:get_body_as_string())
+    self._soup:queue_message(msg, function(_, msg, _)
+        local body = msg.response_body:flatten():get_data()
 
-
-    local entries = {}
-    for _, entry in pairs(json.decode(body)) do
-        if not self._ignore_projects[entry.pid] then
-            entries[entry.id] = entry
+        local entries = {}
+        for _, entry in pairs(json.decode(body)) do
+            if not self._ignore_projects[entry.pid] then
+                entries[entry.id] = entry
+            end
         end
-    end
 
-    self._entries = entries
+        self._entries = entries
+    end)
 end
 
 function toggl:getTotalTimeToday()
